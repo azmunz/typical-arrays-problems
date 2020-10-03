cd6e8d953c38253460dd96bc5a9a102466dab21d
Yuri Nesterov <azmun.ra@gmail.com>
2020-10-03 12:27:04 +0300

    init: typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+exports.min = function min(array) {
+    if (array.length === 0) {
+        return 0;
+    }
+    let x = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (x > array[i]) {
+            x = array[i];
+        }
+    }
+    return x;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array.length === 0) {
+        return 0;
+    }
+    let x = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (x < array[i]) {
+            x = array[i];
+        }
+    }
+    return x;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array.length === 0) {
+        return 0;
+    }
+    let x = 0;
+    for (let i = 0; i < array.length; i++) {
+        x = x + arr[i];
+    }
+    return x / array.length;
+};
